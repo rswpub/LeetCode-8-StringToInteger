4d2c893856a16fdf53f8578d174a4a5cef88c9c8
RSW <rswpub@gmail.com>
2022-07-12 14:18:53 -0600

    Updates

--- LeetCode-8-StringToInteger/Class1.cs
+++ LeetCode-8-StringToInteger/Class1.cs
@@ -2,6 +2,71 @@
 {
     public class Class1
     {
+        public int MyAtoi(string s)
+        {
+            /*
+            The algorithm for myAtoi(string s) is as follows:
+            - Read in and ignore any leading whitespace.
+            - Check if the next character(if not already at the end of the string) is '-' or '+'.Read this character in if it is either.This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+            - Read in next the characters until the next non - digit character or the end of the input is reached.The rest of the string is ignored.
+            - Convert these digits into an integer(i.e. "123"-> 123, "0032"-> 32). If no digits were read, then the integer is 0.Change the sign as necessary (from step 2).
+            - If the integer is out of the 32 - bit signed integer range[-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than - 231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+            - Return the integer as the final result.
+            */
+
+            // Get rid of any leading whitespace
+            s = s.TrimStart();
+
+            // Check for any '-' or '+' sign
+            bool isNegative = false;
+            if ((s.Length >= 1) && ((s[0] == '-') || (s[0] == '+')))
+            {
+                if (s[0] == '-')
+                {
+                    isNegative = true;
+                }
+
+                // Remove the leading sign
+                s = s.Substring(1);
+            }
+
+            // Read the remaining characters one by one until there are no numeric digits left
+            string numPartString = string.Empty;
+            bool endOfDigitsReached = false;
+            for (int i = 0; i < s.Length && !endOfDigitsReached; i++)
+            {
+                switch (s[i])
+                {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        // Collect all the numeric digits
+                        numPartString += s[i];
+                        break;
+                    default:
+                        endOfDigitsReached = true;
+                        break;
+                }
+            }
+
+            // Convert the digits into a 32-bit signed integer
+            int convertedNum = Convert.ToInt32(numPartString);
+
+            // Reapply the negative sign, if appropriate
+            if (isNegative)
+            {
+                convertedNum *= -1;
+            }
+
+            return convertedNum;
+        }
 
     }
 }
--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,10 +1,29 @@
+using LeetCode_8_StringToInteger;
+
 namespace TestProject
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("+1", 1)]
+        [InlineData("-1", -1)]
+        [InlineData("  -1", -1)]
+        [InlineData("  -123 Main St.", -123)]
+        [InlineData("  123 Main St.", 123)]
+        [InlineData("  001234 Main St.", 1234)]
+        [InlineData("  +0012345 Main St.", 12345)]
+        [InlineData("+00123456", 123456)]
+        public void ReturnIntegerFromString(string strNum, int expectedResult)
         {
+            // Arrange
+            Class1 class1 = new Class1();
+
+            // Act
+            int actualResult = class1.MyAtoi(strNum);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
 
         }
     }
